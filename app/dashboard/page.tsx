3ab315b105b25889ca8d7ac986e1a97432d4dee7
rlshelton <rlshelton@wpi.edu>
2025-02-20 20:13:16 -0600

    With Run Graph

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,11 @@
 
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
+<<<<<<< Updated upstream
+=======
+import { refreshToken } from '@/utils/oauth';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Label } from 'recharts'; 
+>>>>>>> Stashed changes
 
 interface AthleteData {
   firstname?: string;
@@ -10,6 +15,370 @@ interface AthleteData {
   profile?: string;
 }
 
+<<<<<<< Updated upstream
+=======
+const DISTANCE_TO_MOON = 238900; //miles
+
+
+
+const MoonProgressCircle = () => {
+  const [totalDistance, setTotalDistance] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [runData, setRunData] = useState<{ dates: string[], times: number[] }>({ dates: [], times: [] });
+
+  useEffect(() => {
+    const fetchTotalDistance = async () => {
+      try {
+        let accessToken = localStorage.getItem('strava_access_token');
+        if (!accessToken) {
+          setError('No access token found');
+          return;
+        }
+
+        //store athlete ID/Token
+        let athleteResponse = await fetch('https://www.strava.com/api/v3/athlete', {
+          headers: {
+            'Authorization': `Bearer ${accessToken}`
+          }
+        });
+
+        if (athleteResponse.status === 401) { //handle error 401: token expired
+          const refreshTokenStr = localStorage.getItem('strava_refresh_token');
+          if (!refreshTokenStr) {
+            throw new Error('No refresh token available');
+          }
+
+          const newTokenData = await refreshToken(refreshTokenStr);
+          localStorage.setItem('strava_access_token', newTokenData.access_token);
+          localStorage.setItem('strava_refresh_token', newTokenData.refresh_token);
+          accessToken = newTokenData.access_token;
+          
+          //retry with new token. Only going to retry once to not overuse API calls
+          athleteResponse = await fetch('https://www.strava.com/api/v3/athlete', {
+            headers: {
+              'Authorization': `Bearer ${accessToken}`
+            }
+          });
+        }
+
+        if (!athleteResponse.ok) {
+          throw new Error('Failed to fetch athlete data');
+        }
+
+        const athleteData = await athleteResponse.json();
+        const statsResponse = await fetch(
+          `https://www.strava.com/api/v3/athletes/${athleteData.id}/stats`,
+          {
+            headers: {
+              'Authorization': `Bearer ${accessToken}` 
+            }
+          }
+        );
+
+        if (!statsResponse.ok) {
+          throw new Error('Failed to fetch athlete stats');
+        }
+
+        const stats = await statsResponse.json();
+        const totalMiles = stats.all_ride_totals.distance * 0.000621371;  
+        /* This converts total distance in meters to miles. 
+        I was seeing about 1% discrepancy between my data on strava and display on the dashboard.
+        Would like to see this be more accurate*/
+        setTotalDistance(totalMiles);
+        const activitiesResponse = await fetch(`https://www.strava.com/api/v3/athletes/${athleteData.id}/activities`, {
+          headers: {
+            'Authorization': `Bearer ${accessToken}`
+          }
+        });
+        if (!activitiesResponse.ok) {
+          throw new Error('Failed to fetch activities');
+        }
+        const activities = await activitiesResponse.json();
+        const recentRuns = activities.filter((activity: any) => activity.type === 'Run').slice(0, 60); // Get last 60 days of run data
+      const dates = recentRuns.map((activity: any) => new Date(activity.start_date).toLocaleDateString());
+      const times = recentRuns.map((activity: any) => activity.elapsed_time / 60); // Convert to minutes
+
+      // Generate dates for every 10th day within the last 60 days
+      const today = new Date();
+      const generatedDates: string[] = [];  // Explicit type definition for dates
+      const generatedTimes: number[] = [];  // Explicit type definition for times
+
+      for (let i = 0; i < 60; i += 1) {
+        const date = new Date(today);
+        date.setDate(today.getDate() - i); // Get the date for every 10th day
+        generatedDates.push(date.toLocaleDateString());
+        generatedTimes.push(0); // Assume no activity for the day initially
+      }
+
+      // Ensure that we include all the generated dates, even if there's no recorded run for them
+      const finalDates: string[] = [];  // Explicit type definition for finalDates
+      const finalTimes: number[] = [];  // Explicit type definition for finalTimes
+
+      generatedDates.forEach((generatedDate, index) => {
+        const idx = dates.indexOf(generatedDate);
+        if (idx !== -1) {
+          finalDates.push(dates[idx]);
+          finalTimes.push(times[idx]);
+        } else {
+          finalDates.push(generatedDate);
+          finalTimes.push(0); // No activity for this day
+        }
+      });
+
+      // Reverse the final arrays so that today's date is on the bottom right
+      finalDates.reverse();
+      finalTimes.reverse();
+
+      setRunData({ dates: finalDates, times: finalTimes });
+
+
+      } catch (error) {
+        console.error('Error fetching stats:', error);
+        setError(error instanceof Error ? error.message : 'Failed to fetch stats');
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchTotalDistance();
+  }, []);
+
+  
+  const progress = (totalDistance / DISTANCE_TO_MOON) * 100; //your progress divided by total distance in %
+  
+  
+  const radius = 90;
+  const circumference = 2 * Math.PI * radius; //there's gotta be a more concise way to do this lol
+  const progressOffset = circumference - (progress / 100) * circumference;
+
+  if (loading) {
+    return <div className="w-96 h-96 flex items-center justify-center">Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div className="w-96 h-96 flex items-center justify-center text-red-500 text-center">
+        {error}
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex space-x-8">
+      <div className="relative w-96 h-96 flex items-center justify-center">
+        {/* Background circle */}
+        <svg className="w-full h-full transform -rotate-90">
+          <circle
+            cx="192"
+            cy="192"
+            r={radius}
+            stroke="currentColor"
+            strokeWidth="36"
+            fill="transparent"
+            className="text-orange-100"
+          />
+          {/* Progress circle */}
+          <circle
+            cx="192"
+            cy="192"
+            r={radius}
+            stroke="currentColor"
+            strokeWidth="36"
+            fill="transparent"
+            strokeDasharray={circumference}
+            strokeDashoffset={progressOffset}
+            className="text-[#FC4C02]"
+            strokeLinecap="round"
+          />
+        </svg>
+        {/* Center text */}
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
+          <span className="text-3xl font-bold text-gray-800 mb-4">
+            {progress.toFixed(2)}%
+          </span>
+        </div>
+        {/* Bottom text */}
+        <div className="absolute bottom-4 left-0 right-0 text-center">
+          <div className="text-sm text-gray-600">
+            <div>Ride to the moon progress:</div>
+            <div>{Math.round(totalDistance).toLocaleString()}/{DISTANCE_TO_MOON.toLocaleString()} miles</div>
+          </div>
+        </div>
+      </div>
+
+      {/* Line Chart for daily running time */}
+      <div className="w-96 h-96">
+      <ResponsiveContainer width="140%" height="70%" style={{ marginTop: '40px' }}>
+        <LineChart data={runData.dates.map((date, index) => ({ date, time: runData.times[index] }))}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey="date"
+            interval={0}  // Ensures every date is shown, but we filter ticks below
+            ticks={runData.dates.filter((_, index) => index % 10 === 0)}  // Filter every 10th day for tick marks
+            tickFormatter={(value) => value}  // Optional: Format dates if needed
+            tick={{ fontSize: 7 }} 
+          >
+            <Label value="Date" position="bottom" offset={-5} /> {/* X-axis label */}
+          </XAxis>
+          <YAxis>
+            <Label value="Time (minutes)" angle={-90} position="left" offset={-2} /> {/* Y-axis label */}
+          </YAxis>
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="time" stroke="#FC4C02" />
+        </LineChart>
+      </ResponsiveContainer>
+        {/*Description below the chart */}
+        <div className="text-center text-sm text-gray-600 mt-4" style={{ marginLeft: '200px', marginBottom: '40px'  }}>
+          This graph shows the time you spent running each day over the past 60 days.
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+
+
+interface Activity {
+  name: string;
+  distance: number; // distance in meters (will convert to miles)
+  type: string;
+}
+
+const TopActivities = () => {
+  const [topRides, setTopRides] = useState<{ name: string; distance: number }[]>([]);
+  const [topRuns, setTopRuns] = useState<{ name: string; distance: number }[]>([]);
+  const [topSwims, setTopSwims] = useState<{ name: string; distance: number }[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchTopActivities = async () => {
+      try {
+        const accessToken = localStorage.getItem('strava_access_token');
+        if (!accessToken) {
+          setError('No access token found');
+          return;
+        }
+
+        const activitiesResponse = await fetch('https://www.strava.com/api/v3/athlete/activities', {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        if (!activitiesResponse.ok) {
+          throw new Error('Failed to fetch activities');
+        }
+
+        const activities: Activity[] = await activitiesResponse.json();
+
+        const rides = activities
+          .filter((activity) => activity.type === 'Ride')
+          .map((ride) => ({
+            name: ride.name,
+            distance: ride.distance * 0.000621371,
+          }))
+          .sort((a: { distance: number }, b: { distance: number }) => b.distance - a.distance)
+          .slice(0, 3);
+
+        const runs = activities
+          .filter((activity) => activity.type === 'Run')
+          .map((run) => ({
+            name: run.name,
+            distance: run.distance * 0.000621371,
+          }))
+          .sort((a: { distance: number }, b: { distance: number }) => b.distance - a.distance)
+          .slice(0, 3);
+
+        const swims = activities
+          .filter((activity) => activity.type === 'Swim')
+          .map((swim) => ({
+            name: swim.name,
+            distance: swim.distance * 0.000621371,
+          }))
+          .sort((a: { distance: number }, b: { distance: number }) => b.distance - a.distance)
+          .slice(0, 3);
+
+        setTopRides(rides);
+        setTopRuns(runs);
+        setTopSwims(swims);
+      } catch (error) {
+        console.error('Error fetching activities:', error);
+        setError(error instanceof Error ? error.message : 'Failed to fetch activities');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTopActivities();
+  }, []);
+
+  if (loading) {
+    return <div>Loading top activities...</div>;
+  }
+
+  if (error) {
+    return <div className="text-red-500">{error}</div>;
+  }
+
+  const ActivityList = ({
+    title,
+    activities,
+  }: {
+    title: string;
+    activities: { name: string; distance: number }[];
+  }) => (
+    <div className="mb-6">
+      <h3 className="text-xl font-bold text-gray-800 mb-4 border-b pb-2">{title}</h3>
+      <ul className="space-y-4">
+        {activities.map((activity, index) => (
+          <li
+            key={index}
+            className="flex items-center justify-between bg-gray-50 rounded-lg p-4 shadow-sm hover:shadow-md transition duration-300"
+          >
+            <div className="flex items-center">
+              <span
+                className="text-lg font-bold bg-[#FC4C02] text-white rounded-full h-8 w-8 flex items-center justify-center mr-4"
+              >
+                {index + 1}
+              </span>
+              <div>
+                <p className="text-gray-800 font-semibold">{activity.name}</p>
+                <p className="text-sm text-gray-500">
+                  Distance: {activity.distance.toFixed(2)} miles
+                </p>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+
+  return (
+    <div className="bg-white rounded-lg shadow-md p-6 grid grid-cols-3 gap-6">
+      <ActivityList title="Top 3 Rides" activities={topRides} />
+      <ActivityList title="Top 3 Runs" activities={topRuns} />
+      <ActivityList title="Top 3 Swims" activities={topSwims} />
+    </div>
+  );
+};
+
+
+
+
+
+const MoonProgressDisplay = () => {
+  return (
+    <div className="bg-white rounded-lg">
+      <MoonProgressCircle />
+    </div>
+  );
+};
+
+>>>>>>> Stashed changes
 export default function Dashboard() {
   const [athlete, setAthlete] = useState<AthleteData | null>(null);
   const [loading, setLoading] = useState(true);
